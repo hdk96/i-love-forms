29728fbbcd39fdab41afc89c12e1cee5a137b1c0
hdk96 <amir.madi34@gmail.com>
2018-08-01 01:05:25 +0000

    imporved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,58 @@
 class UsersController < ApplicationController
    
+   before_action :set_new, only: [:new, :new2, :new3]
+   before_action :set_create, only: [:create, :create2]
+   
    def new
-       @user = User.new
+   end
+   
+   def new2
+   end
+   
+   def new3
+   end
+   
+   def info
+       @user = User.last
    end
    
    def create
-        
-        puts params
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to root_path
-        end
+   end
+   def create2
+   end
+   
+   def create3
        
+       @user = User.new(user_params3)
+        if @user.save
+            redirect_to info_path
+        end
    end
    
    private
    
+
+    def set_new
+         @user = User.new
+    end
+    
+    def set_create
+          
+       @user = User.new(user_params)
+        if @user.save
+            redirect_to info_path
+        end
+        
+    end
+   
     def user_params
         
-        params.permit(:username, :email, :bio)
+         params.permit(:username, :email, :bio)
   end
+  
+  def user_params3
+      
+      params.require(:user).permit(:username, :email, :bio)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/new', to:"users#new"
   post '/new', to: 'users#create'
+  get '/new2', to:"users#new2"
+  post '/new2', to: 'users#create2'
+  get '/new3', to:"users#new3"
+  post '/new3', to: 'users#create3'
+  get '/info', to:'users#info'
 
 end
